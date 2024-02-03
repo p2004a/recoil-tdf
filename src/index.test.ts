93fe7d99551e69f5d0cdd0241766d3412b5bf675
Marek Rusinowski <marekrusinowski@gmail.com>
2024-02-03 23:52:08 +0100

    Add parsing tests against reference implementation

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,36 @@
 import test from 'node:test';
 import assert from 'node:assert/strict';
+import path from 'node:path';
+import { Lua } from 'wasmoon-lua5.1';
+import * as fs from 'node:fs/promises';
 
-import { hello } from './index.js';
+import { parse, TDFObj } from './index.js';
 
-test('hello', () => {
-	assert.equal(hello(), 'hello');
+const lua = await Lua.create();
+
+interface LuaTDFparser {
+	ParseText(data: string): TDFObj | null;
+}
+
+lua.doStringSync(await fs.readFile('testdata/parse_tdf.lua', { encoding: 'utf-8' }));
+
+const luaTDFParser = lua.global.get('TDFparser') as LuaTDFparser;
+
+test('examples parsing', async (t) => {
+	const examples = await fs.readdir('testdata/examples');
+	for (const exampleName of examples) {
+		await t.test(exampleName, async () => {
+			const tdf = await fs.readFile(
+				path.join('testdata/examples', exampleName),
+				{ encoding: 'utf-8' });
+			const expected = luaTDFParser.ParseText(tdf);
+			let actual = null;
+			try {
+				actual = parse(tdf);
+			} catch (e) {
+				// pass
+			}
+			assert.deepEqual(actual, expected);
+		});
+	}
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export function hello(): string {
-	return 'hello';
+export type TDFObj = { [k: string]: (TDFObj | string) };
+
+export function parse(tdf: string): TDFObj {
+	return {};
 }
